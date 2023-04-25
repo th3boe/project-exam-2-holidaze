9ed3227b0548c090eed33e9d8a60f624e27dc907
Boe <boe3am@gmail.com>
2023-04-25 13:48:33 +0200

    API

---
+++ src/constants/api.jsx
@@ -0,0 +1,6 @@
+export const API_HOST_URL = "https://api.noroff.dev";
+export const API_BASE = "/api/v1";
+export const API_HOLIDAZE_BASE = "/holidaze";
+export const API_HOLIDAZE_URL = `${API_HOST_URL}${API_BASE}${API_HOLIDAZE_BASE}`;
+
+export const TOKEN_AUTH = "/auth";
