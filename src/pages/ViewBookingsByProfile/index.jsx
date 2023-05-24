2c2731400a4f26d8e632ba5d18ca5244397ab754
Boe <boe3am@gmail.com>
2023-05-24 15:19:54 +0200

    view bookings your upcoming bookings

---
+++ src/pages/ViewBookingsByProfile/index.jsx
@@ -0,0 +1,139 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_HOLIDAZE_URL } from "../../constants/api";
+import AuthContext from "../../context/AuthContext";
+import useAxios from "../../hooks/useAxios";
+import { BsPersonFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
+import styles from "./bookingsbyprofile.module.css";
+import Carousel from "react-bootstrap/Carousel";
+import PlaceholderImage from "../../images/placeholder.jpg";
+import Button from "../../components/Button";
+
+// Venues function
+
+export default function MyBookings() {
+  const [loader, setLoader] = useState(false);
+  const [upsError, setUpsError] = useState(false);
+  const [bookings, setBookings] = useState([]);
+  const [authenticate] = useContext(AuthContext);
+
+  // Navigate function, and function to navigate to profile
+
+  const navigate = useNavigate();
+  const handleOnClickProfile = () => {
+    navigate(`/profile/${authenticate.name}`);
+  };
+  const handleOnClickVenues = () => {
+    navigate("/venues");
+  };
+
+  // URL
+
+  const action = "/profiles";
+  const http = useAxios();
+  const URL = `${API_HOLIDAZE_URL}${action}/${authenticate.name}?_bookings=true`;
+
+  // Fetch for the bookings
+
+  useEffect(() => {
+    async function getBookings() {
+      try {
+        setUpsError(false);
+        setLoader(true);
+
+        const response = await http.get(URL);
+        console.log(response.data);
+
+        setBookings(response.data);
+        setLoader(false);
+      } catch (error) {
+        setLoader(false);
+        setUpsError(true);
+      }
+    }
+    getBookings();
+  }, []);
+
+  // Content for the above try and catch!
+
+  if (loader) {
+    return (
+      <div className={styles.loader}>
+        <Spinner animation="border" />
+        Loading venues...
+      </div>
+    );
+  }
+
+  if (upsError) {
+    return (
+      <div className={styles.errorMessage}>
+        Oh no.. There seems to be a problem, please hang on while we look into
+        it!
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className={styles.topOfPage}>
+        <div className={styles.title}>
+          <h1>All {authenticate.name}'s upcoming Bookings</h1>
+        </div>
+        <div>
+          <Button
+            name={"Return to Profile"}
+            onClick={() => handleOnClickProfile()}
+          />
+          <Button
+            name={"Find a Destination"}
+            onClick={() => handleOnClickVenues()}
+          />
+        </div>
+      </div>
+      <div className={styles.cardContainer}>
+        {bookings.length === 0 ? (
+          <p>You have no bookings yet.</p>
+        ) : (
+          <>
+            {bookings.bookings.map((booking) => (
+              <div key={booking.id}>
+                <div className={styles.bookingCard}>
+                  <h2>Your booking for {booking.venue.name}</h2>
+                  <div className={styles.cardInformation}>
+                    <p>Your stay is set to happen:</p>
+                    <p>
+                      {new Date(booking.dateFrom).toDateString()} until{" "}
+                      {new Date(booking.dateTo).toDateString()}
+                    </p>
+                    <p>
+                      Booked for{" "}
+                      {booking.guests > 1 ? (
+                        <>{booking.guests} people</>
+                      ) : (
+                        <>{booking.guests} person</>
+                      )}
+                    </p>
+                  </div>
+                  <div className={styles.button}>
+                    <Button
+                      name={"Go to Venue"}
+                      onClick={() => {
+                        navigate("/venue" + `/${booking.venue.id}`);
+                      }}
+                    />
+                  </div>
+                  <p className={styles.cardFooter}>
+                    Booked at {new Date(booking.created).toDateString()}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
