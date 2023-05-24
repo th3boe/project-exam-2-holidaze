e3322f16a62200d6e0c0e8d4b9764e251b940959
Boe <boe3am@gmail.com>
2023-05-24 15:12:39 +0200

    header and nav updated! added selected nav etc.

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 
 import Container from "react-bootstrap/Container";
+import Logo from "../../images/logo-desktop.png";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import AuthContext from "../../context/AuthContext";
@@ -16,46 +17,118 @@ export default function HeaderNav() {
     setAuthenticate(null);
   };
   return (
-    <div className={styles.nav}>
-      <Navbar expand="sm" variant="dark">
-        <Container className="justify-content-end">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className="justify-content-center"
-          >
-            <Nav>
-              <NavLink to="/">
-                <FaHome />
-              </NavLink>
-              <NavLink to="/venues">Venues</NavLink>
-            </Nav>
+    // <div className={styles.nav}>
+    <Navbar
+      collapseOnSelect
+      expand="sm"
+      variant="dark"
+      className={styles.header}
+    >
+      <Container className={styles.headerPlacement}>
+        <Navbar.Brand to="/">
+          <img
+            className={styles.logoImage}
+            height="100vmin"
+            src={Logo}
+            alt="Holidaze-logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {/* <Nav className="me-auto">
+            {" "}
+            <NavLink to="/">
+              {/* Home <FaHome />
+              Home{" "}
+            </NavLink>
+            <NavLink to="/venues">Venues</NavLink>{" "}
+          </Nav> */}
+          {/* <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav> */}
+          {/* <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav> */}
 
-            {authenticate ? (
-              <Nav>
-                <NavLink to={"/profile" + `/${authenticate.name}`}>
-                  Signed in as <CgProfile />
-                  {""} {authenticate.name}
+          {authenticate ? (
+            <>
+              <Nav className="me-auto">
+                <NavLink
+                  to="/"
+                  className={(navigation) =>
+                    navigation.isActive ? styles.active : ""
+                  }
+                >
+                  Home{" "}
+                </NavLink>
+                <NavLink
+                  to="/venues"
+                  className={(navigation) =>
+                    navigation.isActive ? styles.active : ""
+                  }
+                >
+                  Venues
+                </NavLink>
+                <NavLink
+                  to={"/profile" + `/${authenticate.name}`}
+                  className={(navigation) =>
+                    navigation.isActive ? styles.active : ""
+                  }
+                >
+                  {/* Signed in as <CgProfile />
+                  {""} {authenticate.name} */}
+                  Profile
                 </NavLink>
                 {/* <NavLink to="/profile">Profile</NavLink> */}
-                <NavLink
-                  className="justify-content-end"
-                  onClick={signout}
-                  to="/"
-                >
+              </Nav>
+              <Nav className={styles.signInOut}>
+                <NavLink onClick={signout} to="/">
                   Sign Out
                 </NavLink>
               </Nav>
-            ) : (
+            </>
+          ) : (
+            <>
+              <Nav className="me-auto">
+                <NavLink
+                  to="/"
+                  className={(navigation) =>
+                    navigation.isActive ? styles.active : ""
+                  }
+                >
+                  {/* Home <FaHome /> */}
+                  Home{" "}
+                </NavLink>
+                <NavLink
+                  to="/venues"
+                  className={(navigation) =>
+                    navigation.isActive ? styles.active : ""
+                  }
+                >
+                  Venues
+                </NavLink>
+                <NavLink
+                  to="/register"
+                  className={(navigation) =>
+                    navigation.isActive ? styles.active : ""
+                  }
+                >
+                  Register
+                </NavLink>
+              </Nav>
               <Nav>
-                <NavLink to="/register">Register</NavLink>
                 <NavLink to="/signin">Sign In</NavLink>
               </Nav>
-            )}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+            </>
+          )}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    // </div>
   );
 }
 
@@ -108,3 +181,48 @@ export default function HeaderNav() {
 //     </>
 //   );
 // }
+
+// HELLO - here!
+
+//  <Navbar expand="sm" variant="dark">
+//    <Container className="justify-content-end">
+//      {/* <Navbar.Brand to="/">React-Bootstrap</Navbar.Brand> */}
+//      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+//        <Nav>
+//          <NavLink to="/">
+//            {/* Home <FaHome /> */}
+//            Home
+//          </NavLink>
+//          <NavLink to="/venues">Venues</NavLink>
+//        </Nav>
+
+//  {authenticate ? (
+//    <>
+//      <Nav>
+//        <NavLink to={"/profile" + `/${authenticate.name}`}>
+//          {/* Signed in as <CgProfile />
+//             {""} {authenticate.name} */}
+//          Profile
+//        </NavLink>
+//        {/* <NavLink to="/profile">Profile</NavLink> */}
+//      </Nav>
+//      <Nav className={styles.signInOut}>
+//        <NavLink className={styles.signInOut} onClick={signout} to="/">
+//          Sign Out
+//        </NavLink>
+//      </Nav>
+//    </>
+//  ) : (
+//    <>
+//      <Nav>
+//        <NavLink to="/register">Register</NavLink>
+//      </Nav>
+//      <Nav className={styles.signInOut}>
+//        <NavLink to="/signin">Sign In</NavLink>
+//      </Nav>
+//    </>
+//  )}
+//      </Navbar.Collapse>
+//    </Container>
+//  </Navbar>;
